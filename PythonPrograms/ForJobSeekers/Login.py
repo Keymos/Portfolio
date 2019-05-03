a0a019a70e02e1de0d95a382e10246d7be2ce792
unknown <kerimfathallah@gmail.com>
2019-05-03 14:02:42 +0100

    '.'

--- PythonPrograms/ForJobSeekers/Login.py
+++ PythonPrograms/ForJobSeekers/Login.py
@@ -12,13 +12,19 @@ frame.grid(row=0, column=0, sticky=N+S+E+W)
 Label_Username = Label(frame, text="Username:", padx=10, pady=7)
 Label_Username.grid(row=0, column=0)
 
-TextField_Username = Text(frame, height=1, width=25)
+TextField_Username = Text(frame, height=1, width=20)
 TextField_Username.grid(row=0, column=1)
 
 Label_Password = Label(frame, text="Password:", padx=10, pady=7)
 Label_Password.grid(row=1, column=0)
 
-TextField_Password = Text(frame, height=1, width=25)
+TextField_Password = Text(frame, height=1, width=20)
 TextField_Password.grid(row=1, column=1)
+
+Button_Login = Button(frame, text='Log In', padx=5)#, command=insert_week)
+Button_Login.grid(row=0, column=2, columnspan=2)
+
+Button_Register = Button(frame, text='Register')#, command=insert_week)
+Button_Register.grid(row=1, column=2, columnspan=2)
 
 root.mainloop()
