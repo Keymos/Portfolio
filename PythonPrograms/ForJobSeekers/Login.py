49ce74e201d8aaba76673b211d07d3cd632b5ade
unknown <kerimfathallah@gmail.com>
2019-05-03 17:40:36 +0100

    '.'

--- PythonPrograms/ForJobSeekers/Login.py
+++ PythonPrograms/ForJobSeekers/Login.py
@@ -1,13 +1,64 @@
 from tkinter import *
 from tkinter import ttk
 import subprocess
+import os
 
 
-def Login_Admin():
+def file_len(fname):
+	# Number of lines in a file
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+
+def Login():
+	CurrentUser = [TextField_Username.get("1.0", END).strip('\n'), TextField_Password.get("1.0", END).strip('\n')]
+	UsersDatabase = open('UsersDatabase.txt','r')
 	if TextField_Username.get("1.0", END).strip('\n') == 'admin':
 		if TextField_Password.get("1.0", END).strip('\n') == 'admin':
 			subprocess.run(["Python", "AdminPanel.py"])
-	
+		else:
+			print('Error')
+	elif CurrentUser in UserDataBaseList:
+		UsersDatabase.close()
+		CurrentUserFile = open('CurrentUserFile.txt', 'w+')
+		CurrentUserFile.write(str(CurrentUser))
+		CurrentUserFile.close()
+		subprocess.run(["Python", "UserPanel.py"])
+
+
+def Register():
+	CurrentUser = [TextField_Username.get("1.0", END).strip('\n'), TextField_Password.get("1.0", END).strip('\n')]
+	if CurrentUser in UserDataBaseList:
+		print('Error')
+	else:
+		UserDataBaseList.append(CurrentUser)
+		TotalUsers =+ 1
+		UsersDatabase = open('UsersDatabase.txt','a+')
+		y = str('\n' + TextField_Username.get("1.0", END).strip('\n') + '; ' + TextField_Password.get("1.0", END).strip('\n'))
+		UsersDatabase.write(y)
+		UserDataBase.close()
+
+# Users' Informations retreival:
+
+if os.path.isfile('UsersDatabase.txt') == False:
+	UsersDatabase = open('UsersDatabase.txt','w+')
+	UsersDatabase.close()
+
+TotalUsers = file_len('UsersDatabase.txt')
+print(TotalUsers)
+
+UsersDatabase = open('UsersDatabase.txt','r+')
+UserDataBaseList = []
+i = 0
+while i < TotalUsers:
+	x =  str(UsersDatabase.readline()).strip('\n').split('; ')
+	UserDataBaseList.append(x)
+	i += 1
+print(UserDataBaseList)
+
+# GUI:
 root = Tk()
 root.title("Hire now")
 
@@ -26,10 +77,10 @@ Label_Password.grid(row=1, column=0)
 TextField_Password = Text(frame, height=1, width=20)
 TextField_Password.grid(row=1, column=1)
 
-Button_Login = Button(frame, text='Log In', padx=5, command=Login_Admin)
+Button_Login = Button(frame, text='Log In', padx=5, command=Login)
 Button_Login.grid(row=0, column=2, columnspan=2)
 
-Button_Register = Button(frame, text='Register')#, command=insert_week)
+Button_Register = Button(frame, text='Register', command=Register)
 Button_Register.grid(row=1, column=2, columnspan=2)
 
 root.mainloop()
---
+++ PythonPrograms/ForJobSeekers/UserPanel.py
@@ -0,0 +1,2 @@
+print('This is the user panel, for now.')
+print('bye')
