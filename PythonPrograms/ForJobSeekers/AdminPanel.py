2770cd4843845afb70795b50b4c67ec36e00a100
unknown <kerimfathallah@gmail.com>
2019-05-04 22:53:03 +0100

    '.'

--- PythonPrograms/ForJobSeekers/AdminPanel.py
+++ PythonPrograms/ForJobSeekers/AdminPanel.py
@@ -2,12 +2,11 @@ from tkinter import *
 from tkinter import ttk
 
 
-
-
-def GridForget():
-	Emptyfield.grid_forget()
-
 def Grid_AddJobOffer_Show():
+	Frame_BrowseAndUpdate.grid_forget()
+	Frame_SearchJobSeekers.grid_forget()
+	SearchJob_SearchButton.grid_forget()
+	SearchButton2.grid_forget()
 	Frame_AddJobOffer.grid(row=1, columnspan=4)
 	AddJobOffer_JobID_Label.grid(row=0, column=0)
 	AddJobOffer_JobID.grid(row=0, column=1, sticky=W)
@@ -30,27 +29,57 @@ def Grid_AddJobOffer_Show():
 	Mission_Label.grid(row=10, column=0)
 	Mission.grid(row=10, column=1, columnspan=3)
 
+def Grid_BrowseAndUpdate_Show():
+	DeleteJobOfferButton.grid_forget()
+	Frame_BrowseAndUpdate.grid_forget()
+	Frame_AddJobOffer.grid_forget()
+	Frame_SearchJobSeekers.grid_forget()
+	Frame_BrowseAndUpdate.grid(row=1, columnspan=4)
+	SearchJobID_Label.grid(row=0, column=0, sticky=W)
+	SearchJobID.grid(row=0, column=1, columnspan=3)
+	SearchJob_SearchButton.grid(row=0, column=4)
 
+def SearchJob():
+	Grid_AddJobOffer_Show()
+	SearchButton2.grid(row=0, column=1, sticky=E)
+
+def Grid_DeleteJobOffer_Show():
+	Frame_AddJobOffer.grid_forget()
+	SearchJob_SearchButton.grid_forget()
+	Frame_SearchJobSeekers.grid_forget()
+	Frame_BrowseAndUpdate.grid(row=1, columnspan=4)
+	SearchJobID_Label.grid(row=0, column=0, sticky=W)
+	SearchJobID.grid(row=0, column=1, columnspan=3)
+	DeleteJobOfferButton.grid(row=0, column=4)
+
+def Grid_SearchJobSeekers_Show():
+	DeleteJobOfferButton.grid_forget()
+	Frame_BrowseAndUpdate.grid_forget()
+	Frame_AddJobOffer.grid_forget()
+	Frame_SearchJobSeekers.grid(row=1, columnspan=4)
+	SearchBy_Label.grid(row=1, column=0)
+	SearchAll_Button.grid(row=2, column=0)
+	SearchByJobOfferAppliedTo_Button.grid(row=2, column=1)
+	SearchByJob_TextField.grid(row=2, column=2)
 
 root = Tk()
 root.title("Hire now !")
-
 
 frame = Frame(root)
 frame.grid(row=0, column=0)
 
 Button_AddJobOffer = Button(frame, text='Add Job Offer', width=25, command=Grid_AddJobOffer_Show)
 Button_AddJobOffer.grid(row=0, column=0)
-Button_BrowseJobOffers = Button(frame, text='Browse Job Offers', width=25)
+Button_BrowseJobOffers = Button(frame, text='Browse Job Offers', width=25, command=Grid_BrowseAndUpdate_Show)
 Button_BrowseJobOffers.grid(row=0, column=1)
-Button_DeleteJobOffer = Button(frame, text='Delete Job Offer', width=25)
+Button_DeleteJobOffer = Button(frame, text='Delete Job Offer', width=25, command=Grid_DeleteJobOffer_Show)
 Button_DeleteJobOffer.grid(row=0, column=2)
-Button_BrowseListOfJobSeekers = Button(frame, text='Browse List Of Job Seekers', width=25)
+Button_BrowseListOfJobSeekers = Button(frame, text='Browse List Of Job Seekers', width=25, command=Grid_SearchJobSeekers_Show)
 Button_BrowseListOfJobSeekers.grid(row=0, column=3)
 Emptyfield = Label(frame, height=32)
 Emptyfield.grid(row=1)
 
-
+# AddJobOffer
 Frame_AddJobOffer = Frame(frame)
 
 AddJobOffer_JobID_Label = Label(Frame_AddJobOffer, text="Job ID")
@@ -76,6 +105,23 @@ Experience = Text(Frame_AddJobOffer, height=5, width=75)
 Mission_Label = Label(Frame_AddJobOffer, text="Mission:")
 Mission = Text(Frame_AddJobOffer, height=5, width=75)
 
-# Frame_AddJobOffer.grid_forget()
+SearchButton2 = Button(Frame_AddJobOffer, text="Search", command=SearchJob)
+
+
+# Browse and update
+Frame_BrowseAndUpdate = Frame(frame)
+
+SearchJobID_Label = Label(Frame_BrowseAndUpdate, text="Job ID:")
+SearchJobID = Text(Frame_BrowseAndUpdate, height=1, width=25)
+SearchJob_SearchButton = Button(Frame_BrowseAndUpdate, text="Search", command=SearchJob)
+
+DeleteJobOfferButton = Button (Frame_BrowseAndUpdate, text="Delete")#, command=
+
+Frame_SearchJobSeekers = Frame(frame)
+SearchBy_Label = Label(Frame_SearchJobSeekers, text="Search by:")
+SearchAll_Button = Button(Frame_SearchJobSeekers, text="All")
+SearchByJobOfferAppliedTo_Button = Button(Frame_SearchJobSeekers,text="Search by job ID:")
+SearchByJob_TextField = Text(Frame_SearchJobSeekers, height=1, width=25)
+
 
 root.mainloop()
